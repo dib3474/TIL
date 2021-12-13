7f7b4e00778700f5bf96a8b764924b6bb10d577c
dib3474 <dib3474@gmail.com>
2021-12-13 19:05:52 +0900

    Add test case

---
+++ test.spec.js
@@ -0,0 +1,9 @@
+describe('Default Test Set', () => {
+    it('text1 should be passed', () => {
+        console.log('test1 passed')
+    });
+
+    it('text2 should be passed', () => {
+        console.log('test2 passed')
+    });
+});
