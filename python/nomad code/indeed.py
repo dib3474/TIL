85a6a336d13f56e0a4f69129e120463d18749beb
dib3474 <dib3474@gmail.com>
2022-01-05 23:47:25 +0900

    Nomadcoder-python indeed fin

--- python/nomad code/indeed.py
+++ python/nomad code/indeed.py
@@ -20,9 +20,32 @@ def extract_indeed_pages():
   max_page = pages[-1]
   return max_page
 
+def extract_job(html):
+  job_title = html.find("h2",{"class": "jobTitle"}).find("span", title=True).string
+
+  company = html.find("span", {"class": "companyName"}).string
+  #if title == "new":
+  #  job_tilte = title.find_all("span")[1].string
+  location = html.select_one("pre > div").text
+  #location = html.find("div", {"class": "companyLocation"}).string  - > 이름이 긴 경우 None이 됨.
+
+  job_id = html.parent['data-jk']
+
+  return {
+    'title': job_title, 
+    'company': company, 
+    'location': location,
+    'link': f"https://www.indeed.com/viewjob?jk={job_id} "
+  }
+
 def extract_indeed_jobs(last_page):
   jobs = []
   for page in range(last_page):
+    print(f"Scrapping page {page}")
     result = requests.get(f"{URL}&start={page*LIMIT}")
-    print(result.status_code)
+    soup = BeautifulSoup(result.text, "html.parser")
+    results = soup.find_all("div", {"class": "slider_container"})
+    for title in results:
+      job = extract_job(title)
+      jobs.append(job)
   return jobs
